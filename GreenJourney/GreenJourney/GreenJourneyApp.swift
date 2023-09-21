552794fcc1820653fc1d20b2bda3f65f1f1f29bd
heidi <heidialbara>
2023-09-21 21:45:36 +0200

    initial commit

---
+++ GreenJourney/GreenJourney/GreenJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenJourneyApp.swift
+//  GreenJourney
+//
+//  Created by Heidi Albarazi on 21.09.23.
+//
+
+import SwiftUI
+
+@main
+struct GreenJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
